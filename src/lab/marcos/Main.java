ba3849657db1161cc3ae87cc34c44bb423a2e3e0
Marcos Daniel <marcos.mdbbr@gmail.com>
2021-10-18 23:14:42 -0300

    initial commit, use httpclient

---
+++ src/lab/marcos/Main.java
@@ -0,0 +1,29 @@
+package lab.marcos;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        connectAndPrintUrlJavaOracle();
+    }
+
+    private static void connectAndPrintUrlJavaOracle() throws IOException, InterruptedException {
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET().uri(URI.create("https://docs.oracle.com/javase/10/language/"))
+                .build();
+
+        HttpClient httpClient = HttpClient.newHttpClient();
+
+        HttpResponse<String> response =  httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println("Status code : " + response.statusCode());
+        System.out.println("Headers : " + response.headers());
+        System.out.println("body : " + response.body());
+    }
+}
